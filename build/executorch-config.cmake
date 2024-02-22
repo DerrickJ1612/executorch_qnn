pytorch/executorch

commit 1bf0fa8ee78952024b1a55a6fe7f0f4953dff4af

Add CMake for llama runner (#2022)
Summary:
Pull Request resolved: https://github.com/pytorch/executorch/pull/2022

As titled. Also add a CI job. Will run a model in next PR.

Reviewed By: mergennachin

Differential Revision: D53996450

fbshipit-source-id: d3a97e18c87c500e85eb7b5b29bca3b1e5967200

build/executorch-config.cmake
@@ -43,7 +43,7 @@ endif()
 
 set(lib_list
     etdump bundled_program extension_data_loader ${FLATCC_LIB} mpsdelegate
-    qnn_executorch_backend
+    qnn_executorch_backend portable_ops_lib extension_module
 )
 foreach(lib ${lib_list})
     # Name of the variable which stores result of the find_library search
@@ -53,7 +53,11 @@ foreach(lib ${lib_list})
         message("${lib} library is not found.
             If needed rebuild with the proper options in CMakeLists.txt")
     else()
-        add_library(${lib} STATIC IMPORTED)
+        if("${lib}" STREQUAL "extension_module")
+            add_library(${lib} SHARED IMPORTED)
+        else()
+            add_library(${lib} STATIC IMPORTED)
+        endif()
         set_target_properties(
             ${lib} PROPERTIES IMPORTED_LOCATION "${${lib_var}}"
         )

extension/module/CMakeLists.txt
@@ -17,13 +17,15 @@ if(NOT EXECUTORCH_ROOT)
 endif()
 
 list(TRANSFORM _extension_module__srcs PREPEND "${EXECUTORCH_ROOT}/")
-add_library(extension_module ${_extension_module__srcs})
+add_library(extension_module SHARED ${_extension_module__srcs})
+target_link_libraries(extension_module PRIVATE executorch)
 target_include_directories(extension_module PUBLIC ${EXECUTORCH_ROOT}/..)
-
+target_compile_options(extension_module PUBLIC -Wno-deprecated-declarations
+                                               -fPIC)
 
 # Install libraries
 install(
   TARGETS extension_module
-  DESTINATION ${CMAKE_BINARY_DIR}/lib
+  DESTINATION lib
   INCLUDES
   DESTINATION ${_common_include_directories})

examples/models/llama2/CMakeLists.txt
@@ -1,0 +1,64 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+#
+# Simple CMake build system for selective build demo.
+#
+# ### Editing this file ###
+#
+# This file should be formatted with
+# ~~~
+# cmake-format --first-comment-is-literal=True CMakeLists.txt
+# ~~~
+# It should also be cmake-lint clean.
+#
+cmake_minimum_required(VERSION 3.19)
+project(llama_runner)
+
+if(NOT PYTHON_EXECUTABLE)
+  set(PYTHON_EXECUTABLE python3)
+endif()
+
+set(EXECUTORCH_ROOT ${CMAKE_CURRENT_SOURCE_DIR}/../../..)
+set(TORCH_ROOT ${EXECUTORCH_ROOT}/third-party/pytorch)
+
+if(NOT CMAKE_CXX_STANDARD)
+  set(CMAKE_CXX_STANDARD 17)
+  # Can't set to 11 due to executor_runner.cpp make_unique
+endif()
+
+set(_common_compile_options -Wno-deprecated-declarations -fPIC)
+
+# Let files say "include <executorch/path/to/header.h>".
+set(_common_include_directories ${EXECUTORCH_ROOT}/..)
+
+find_package(gflags REQUIRED PATHS
+             ${CMAKE_CURRENT_BINARY_DIR}/../../../third-party)
+
+#
+# llama_main: test binary to run llama, with tokenizer and sampler integrated
+#
+add_executable(llama_main main.cpp)
+if(CMAKE_BUILD_TYPE EQUAL "RELEASE")
+  target_link_options(llama_main PRIVATE "LINKER:--gc-sections")
+endif()
+
+# find `executorch` libraries
+find_package(executorch CONFIG REQUIRED)
+
+# llama_runner library
+add_subdirectory(runner)
+
+# XNNPACK pthreadpool cpuinfo
+target_link_libraries(llama_main PUBLIC gflags llama_runner portable_ops_lib)
+target_link_options(
+  llama_main PUBLIC "SHELL:LINKER:--whole-archive \
+                    $<TARGET_FILE:portable_ops_lib> \
+                    LINKER:--no-whole-archive")
+target_compile_options(llama_main PUBLIC ${_common_compile_options})
+
+# Print all summary
+executorch_print_configuration_summary()

examples/models/llama2/runner/CMakeLists.txt
@@ -1,0 +1,40 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+#
+# Simple CMake build system for selective build demo.
+#
+# ### Editing this file ###
+#
+# This file should be formatted with
+# ~~~
+# cmake-format --first-comment-is-literal=True CMakeLists.txt
+# ~~~
+# It should also be cmake-lint clean.
+#
+
+include(${EXECUTORCH_ROOT}/build/Utils.cmake)
+include(${EXECUTORCH_ROOT}/build/Codegen.cmake)
+#
+# The `_<target>_srcs` lists are defined by including ${EXECUTORCH_SRCS_FILE}.
+#
+set(EXECUTORCH_SRCS_FILE
+    "${CMAKE_CURRENT_BINARY_DIR}/../../../../executorch_srcs.cmake")
+
+extract_sources(${EXECUTORCH_SRCS_FILE})
+
+include(${EXECUTORCH_SRCS_FILE})
+
+# build llama_runner library
+list(TRANSFORM _llama_runner__srcs PREPEND "${EXECUTORCH_ROOT}/")
+
+target_include_directories(extension_module
+                           INTERFACE ${_common_include_directories})
+
+add_library(llama_runner SHARED ${_llama_runner__srcs})
+target_link_libraries(
+  llama_runner PUBLIC executorch portable_kernels extension_module
+                      extension_data_loader)

kernels/portable/CMakeLists.txt
@@ -60,4 +60,4 @@ target_compile_options(portable_kernels PUBLIC ${_common_compile_options})
 # runtime
 gen_operators_lib("portable_ops_lib" portable_kernels executorch)
 
-install(TARGETS portable_kernels DESTINATION lib)
+install(TARGETS portable_kernels portable_ops_lib DESTINATION lib)

.github/workflows/pull.yml
@@ -213,3 +213,27 @@ jobs:
     uses: ./.github/workflows/_unittest.yml
     with:
       docker-image: executorch-ubuntu-22.04-clang12
+
+  test-llama-runner-cmake:
+    name: test-llama-runner
+    uses: pytorch/test-infra/.github/workflows/linux_job.yml@main
+    strategy:
+      matrix:
+        include:
+          - build-tool: cmake
+      fail-fast: false
+    with:
+      runner: linux.2xlarge
+      docker-image: executorch-ubuntu-22.04-clang12
+      submodules: 'true'
+      ref: ${{ github.event_name == 'pull_request' && github.event.pull_request.head.sha || github.sha }}
+      timeout: 90
+      script: |
+        # The generic Linux job chooses to use base env, not the one setup by the image
+        CONDA_ENV=$(conda env list --json | jq -r ".envs | .[-1]")
+        conda activate "${CONDA_ENV}"
+
+        BUILD_TOOL=${{ matrix.build-tool }}
+        PYTHON_EXECUTABLE=python bash .ci/scripts/setup-linux.sh "${BUILD_TOOL}"
+        # Test selective build
+        PYTHON_EXECUTABLE=python bash examples/models/llama2/test_llama_runner.sh "${BUILD_TOOL}"

build/cmake_deps.toml
@@ -13,21 +13,7 @@ excludes = [
   "^third-party",
 ]
 
-[targets.executor_runner]
-buck_targets = [
-  "//examples/portable/executor_runner:executor_runner",
-]
-filters = [
-  ".cpp$",
-]
-excludes = [
-  "^codegen",
-]
-deps = [
-  "executorch",
-  "portable_kernels",
-  "quantized_kernels",
-]
+# ---------------------------------- core start ----------------------------------
 
 [targets.executorch]
 buck_targets = [
@@ -40,42 +26,6 @@ filters = [
   ".cpp$",
 ]
 
-[targets.extension_data_loader]
-buck_targets = [
-  "//extension/data_loader:buffer_data_loader",
-  "//extension/data_loader:file_data_loader",
-  "//extension/data_loader:mmap_data_loader",
-  "//extension/data_loader:shared_ptr_data_loader",
-]
-filters = [
-  ".cpp$",
-]
-deps = [
-  "executorch",
-]
-
-[targets.extension_module]
-buck_targets = [
-  "//extension/module:module",
-]
-filters = [
-  ".cpp$",
-]
-deps = [
-  "executorch",
-  "extension_data_loader",
-]
-
-[targets.extension_runner_util]
-buck_targets = [
-  "//extension/runner_util:inputs",
-]
-filters = [
-  ".cpp$",
-]
-deps = [
-  "executorch",
-]
 
 [targets.portable_kernels]
 buck_targets = [
@@ -119,15 +69,63 @@ filters = [
   ".fbs$",
 ]
 
-[targets.print_evalue]
+# ---------------------------------- core end ----------------------------------
+# ---------------------------------- extension start ----------------------------------
+
+[targets.extension_data_loader]
 buck_targets = [
-  "//extension/evalue_util:print_evalue",
+  "//extension/data_loader:buffer_data_loader",
+  "//extension/data_loader:file_data_loader",
+  "//extension/data_loader:mmap_data_loader",
+  "//extension/data_loader:shared_ptr_data_loader",
 ]
 filters = [
   ".cpp$",
 ]
 deps = [
   "executorch",
+]
+
+[targets.extension_module]
+buck_targets = [
+  "//extension/module:module",
+]
+filters = [
+  ".cpp$",
+]
+deps = [
+  "executorch",
+  "extension_data_loader",
+]
+
+[targets.extension_runner_util]
+buck_targets = [
+  "//extension/runner_util:inputs",
+]
+filters = [
+  ".cpp$",
+]
+deps = [
+  "executorch",
+]
+
+# ---------------------------------- extension end ----------------------------------
+# ---------------------------------- binary start ----------------------------------
+
+[targets.executor_runner]
+buck_targets = [
+  "//examples/portable/executor_runner:executor_runner",
+]
+filters = [
+  ".cpp$",
+]
+excludes = [
+  "^codegen",
+]
+deps = [
+  "executorch",
+  "portable_kernels",
+  "quantized_kernels",
 ]
 
 [targets.size_test]
@@ -143,7 +141,8 @@ excludes = [
 deps = [
   "executorch",
 ]
-
+# ---------------------------------- binary end ----------------------------------
+# ---------------------------------- MPS start ----------------------------------
 [targets.mps_executor_runner]
 buck_targets = [
   "//examples/apple/mps/executor_runner:mps_executor_runner",
@@ -177,6 +176,9 @@ buck_targets = [
 filters = [
   ".fbs$",
 ]
+
+# ---------------------------------- MPS end ----------------------------------
+# ---------------------------------- XNNPACK start ----------------------------------
 
 [targets.xnn_executor_runner]
 buck_targets = [
@@ -220,3 +222,24 @@ buck_targets = [
 filters = [
   ".fbs$",
 ]
+# ---------------------------------- XNNPACK end ----------------------------------
+# ---------------------------------- LLama start ----------------------------------
+
+[targets.llama_runner]
+buck_targets = [
+  "//examples/models/llama2/runner:runner",
+]
+filters = [
+  ".cpp$",
+]
+excludes = [
+  "^codegen",
+]
+deps = [
+  "executorch",
+  "extension_module",
+  "portable_kernels",
+  "quantized_kernels",
+  "xnnpack_backend",
+]
+# ---------------------------------- LLama start ----------------------------------

examples/models/llama2/test_llama_runner.sh
@@ -1,0 +1,47 @@
+#!/bin/bash
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+# Test Llama runner in examples/models/llama2/main.cpp
+# 1. Export a llama-like model
+# 2. Build llama runner binary
+# 3. Run model with the llama runner binary with prompt
+set -e
+# shellcheck source=/dev/null
+source "$(dirname "${BASH_SOURCE[0]}")/../../../.ci/scripts/utils.sh"
+
+cmake_install_executorch_libraries() {
+    echo "Installing libexecutorch.a, libextension_module.so, libportable_ops_lib.a"
+    rm -rf cmake-out
+    retry cmake -DBUCK2="$BUCK" \
+        -DCMAKE_INSTALL_PREFIX=cmake-out \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DEXECUTORCH_BUILD_EXTENSION_MODULE=ON \
+        -DEXECUTORCH_BUILD_EXTENSION_DATA_LOADER=ON \
+        -DPYTHON_EXECUTABLE="$PYTHON_EXECUTABLE" \
+        -Bcmake-out .
+    cmake --build cmake-out -j9 --target install --config Release
+}
+
+cmake_build_llama_runner() {
+    echo "Building llama runner"
+    dir="examples/models/llama2"
+    retry cmake -DBUCK2="$BUCK" \
+        -DCMAKE_INSTALL_PREFIX=cmake-out \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DPYTHON_EXECUTABLE="$PYTHON_EXECUTABLE" \
+        -Bcmake-out/${dir} \
+        ${dir}
+    cmake --build cmake-out/${dir} -j9 --config Release
+
+}
+
+if [[ $1 == "cmake" ]];
+then
+    cmake_install_executorch_libraries
+    cmake_build_llama_runner
+    # TODO(larryliu0820): export a model and verify the result
+fi

DerrickJ1612/executorch_qnn

commit 40d6d9578e6c41839640e054e3ca264e10bb43cc

Build static extension_module lib for iOS demo apps (#2038)
Summary:
Building the demo apps on iOS is broken after https://github.com/pytorch/executorch/pull/2022 because it switches the `extension_module` library from a static to a shared one.  AFAIK, the latter requires code signing and it's failing on CI with the following error `Signing for "extension_module" requires a development team`

I'm not quite sure what would be the best way to address this, but keeping the library as a static one for iOS build seems like an easy workaround.

Pull Request resolved: https://github.com/pytorch/executorch/pull/2038

Reviewed By: larryliu0820

Differential Revision: D54047521

Pulled By: huydhn

fbshipit-source-id: e752f29bb1fefd355525a40d586f89659119a625

.github/workflows/app-build.yml
@@ -14,6 +14,7 @@ on:
       - build/build_apple_frameworks.sh
       - build/test_ios_ci.sh
       - examples/demo-apps/**
+      - extension/module/**
   workflow_dispatch:
 
 concurrency:

build/executorch-config.cmake
@@ -53,9 +53,12 @@ foreach(lib ${lib_list})
         message("${lib} library is not found.
             If needed rebuild with the proper options in CMakeLists.txt")
     else()
-        if("${lib}" STREQUAL "extension_module")
+        if("${lib}" STREQUAL "extension_module" AND (NOT CMAKE_TOOLCHAIN_IOS))
             add_library(${lib} SHARED IMPORTED)
         else()
+            # Building a share library on iOS requires code signing, so it's
+            # easier to keep all libs as static when CMAKE_TOOLCHAIN_IOS is
+            # used
             add_library(${lib} STATIC IMPORTED)
         endif()
         set_target_properties(

extension/module/CMakeLists.txt
@@ -17,7 +17,12 @@ if(NOT EXECUTORCH_ROOT)
 endif()
 
 list(TRANSFORM _extension_module__srcs PREPEND "${EXECUTORCH_ROOT}/")
-add_library(extension_module SHARED ${_extension_module__srcs})
+if(CMAKE_TOOLCHAIN_IOS)
+  # Building a share library on iOS requires code signing
+  add_library(extension_module STATIC ${_extension_module__srcs})
+else()
+  add_library(extension_module SHARED ${_extension_module__srcs})
+endif()
 target_link_libraries(extension_module PRIVATE executorch)
 target_include_directories(extension_module PUBLIC ${EXECUTORCH_ROOT}/..)
 target_compile_options(extension_module PUBLIC -Wno-deprecated-declarations